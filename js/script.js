7984e268f019d9db7b83bfc1282b18287e623b7a
ishunter216 <michael.king@edenprogram.com>
2023-05-13 08:23:26 +0300

    mobiel styles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
+// add arrows to nav items with children
+const morelink = document.querySelectorAll('.more-link'),
+      svgArrow = document.createElement('span');
+
+svgArrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" fill="currentColor"/></svg>`;
+console.log(morelink);
+console.log(svgArrow);
+
+morelink.forEach(elem => {
+  console.log(elem)
+  elem.append(svgArrow.cloneNode(true));
+});
+
 // Header screen
 const headerScrollStart = document.querySelector('.js-header').offsetTop,
       headerScrollStop = document.querySelector('.js-header').offsetHeight + headerScrollStart,
